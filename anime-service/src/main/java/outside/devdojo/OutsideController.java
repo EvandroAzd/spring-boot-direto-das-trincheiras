0722266eb9401c860bb366ecc90a635b0ae6358a
evandro <azevedoevandrogabriel@gmail.com>
2026-04-01 12:56:57 -0300

    Aula 08 - Spring Beans e ComponentScan

--- anime-service/src/main/java/outside/devdojo/OutsideController.java
+++ anime-service/src/main/java/outside/devdojo/OutsideController.java
@@ -1,13 +1,13 @@
-package academy.devdojo.controllers;
+package outside.devdojo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class OutsideController {
 
-    @GetMapping("hello")
-    public String hi(){
-        return "Hello world!";
+    @GetMapping("test")
+    public String outside(){
+        return "OutsideController";
     }
 }
